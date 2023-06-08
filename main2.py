a703ff481cad201814208988b96a9d798a1f58d5
BulentEmreMollamehmetoglu <106030451+BulentEmreMollamehmetoglu@users.noreply.github.com>
2023-06-08 22:15:55 +0300

    some changes on main flask file

--- main2.py
+++ main2.py
@@ -80,4 +80,4 @@ def predictSVCGet():
 def route():
     return "hello"
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=1234,debug=True)# debug=True
+    app.run(host='0.0.0.0.',port=5000,debug=True)# debug=True
